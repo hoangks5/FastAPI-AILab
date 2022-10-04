f2fb5d6593c26fc887d34ab3e011ac487b928a9b
hoangks5 <hoangnd.ute@gmail.com>
2022-10-04 16:58:57 +0700

    --- Auto Deply ---

--- app/main.py
+++ app/main.py
@@ -183,14 +183,14 @@ This detector is usually preferred to QuantileAD in the case where only a tiny p
 @app.post("/PersistAD/upload_file",tags=["Abnormal Detection Data Time Series"])
 async def PersistAD_upload_file(in_file: UploadFile=File(...), c: float = Form(3.0,description="""c (float, optional) – Factor used to determine the bound of normal range based on historical interquartile range.
                                                                                
-            Default: 3.0."""), side: str = Form('both',description="""If “both”, to detect anomalous positive and negative changes;
+    Default: 3.0."""), side: str = Form('both',description="""If “both”, to detect anomalous positive and negative changes;
 
 If “positive”, to only detect anomalous positive changes;
 
 If “negative”, to only detect anomalous negative changes.
 
 
-            Default: “both”.""")):
+    Default: “both”.""")):
     """
     PersistAD compares each time series value with its previous values. Internally, it is implemented as a pipenet with transformer DoubleRollingAggregate.
 
