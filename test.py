d03d4f5f0307747c1f08e4dc9615286e35754053
hoangks5 <hoangnd.ute@gmail.com>
2022-10-04 15:48:15 +0700

    --- Auto Deply ---

--- test.py
+++ test.py
@@ -1,2 +1,2 @@
-from underthesea import 
+from underthesea import sent_tokenize
 print(text_normalize('Ừ Thế mày đã nuôi được bố mẹ mày Bữa nào chưa thay Xưa nay vẫn báo hải cơm cha mẹ máy mấy hôm thấy ông đơ mà không thèm nói mày lại làm già'))
