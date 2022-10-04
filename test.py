6e50cf5d79a4da10c1d4f1596135d7f2216e35b6
hoangks5 <hoangnd.ute@gmail.com>
2022-10-04 15:50:15 +0700

    --- Auto Deply ---

--- test.py
+++ test.py
@@ -1,3 +1,2 @@
-from telnetlib import SE
-from underthesea import sent_tokenize
-print(sent_tokenize('Ừ Thế mày đã nuôi được bố mẹ mày Bữa nào chưa thay Xưa nay vẫn báo hải cơm cha mẹ máy mấy hôm thấy ông đơ mà không thèm nói mày lại làm già'))
+from underthesea import text_normalize
+print(text_normalize('Ừ Thế mày đã nuôi được bố mẹ mày Bữa nào chưa thay Xưa nay vẫn báo hải cơm cha mẹ máy mấy hôm thấy ông đơ mà không thèm nói mày lại làm già'))
