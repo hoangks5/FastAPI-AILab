7887922624db703460bf3960c0a4c76c4bd1ed86
hoangks5 <hoangnd.ute@gmail.com>
2022-10-04 16:36:06 +0700

    --- Auto Deply ---

--- test.py
+++ test.py
@@ -1,2 +1,2 @@
 from underthesea import text_normalize
-print(text_normalize('Dạ không trẻ bên em có 2 loại hình loại hình doanh nghiệp dưới 100 người và trên 100 người thôi'))
+print(text_normalize('Dạ không chi bên em có 2 loại hình loại hình doanh nghiệp dưới 100 người và trên 100 người thôi'))
