55b3345f02db0995872bf9c8011469930d84ee12
hoangks5 <hoangnd.ute@gmail.com>
2022-10-04 10:46:34 +0700

    --- Auto Deply ---

--- app/main.py
+++ app/main.py
@@ -187,8 +187,7 @@ async def PersistAD_ipfs_hash(input_source_hash : str = Form(), c: float = Form(
 
 @app.post("/speech_to_text/upload_file",tags=["Speech To Text"])
 async def speech_to_text_upload_file(in_file: UploadFile = File(...) ):
-    data_text = requests.get('https://gateway.ipfs.airight.io/ipfs/'+input_source_hash).content
-    print(type(data_text))
+    data_text = in_file.file()
     import speech_recognition as sr
     r = sr.Recognizer()
     import soundfile
